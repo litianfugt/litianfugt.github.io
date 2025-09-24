5ba52e8a5bd0648c07fd915098e57c00a89b93c3
“litianfugt” <13158789083@163.com>
2025-09-24 16:19:40 +0800

    完善了随想栏的点赞和评论功能 GitHub gist

--- static/github-gist-config.js
+++ static/github-gist-config.js
@@ -2,8 +2,8 @@
 // 将此文件复制到您的网站根目录，并根据您的实际情况修改配置
 
 // GitHub Gist 配置
-window.GITHUB_GIST_TOKEN = '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';        // 替换为您的GitHub个人访问令牌
-window.GITHUB_GIST_ID = '8d83cd81473f787cd064a637b4ddd4d4';                // 替换为您的Gist ID
+window.GITHUB_GIST_TOKEN = '';                        // 请替换为您的GitHub个人访问令牌
+window.GITHUB_GIST_ID = '';                            // 请替换为您的Gist ID
 
 // 创建个人访问令牌: https://github.com/settings/tokens
 // 需要勾选 'gist' 权限
